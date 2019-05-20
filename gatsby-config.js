c3c823c0e807c9f1db21672da92dd23d7982d35f
Josh Head <jhead12@users.noreply.github.com>
2019-05-19 23:03:47 -0700

    Phase2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Matri Test`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `The foundation to the multiple site set up.`,
+    author: `@_joshhead_`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
