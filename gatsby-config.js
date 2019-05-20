8fdc38751b31d7c902fb0229ea921e782907bd4e
Josh Head <jhead12@users.noreply.github.com>
2019-05-20 00:48:08 -0700

    added scss

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,13 +13,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    'gatsby-plugin-sass',
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `matrixblend-starter-default`,
-        short_name: `starter`,
+        short_name: `Tester`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
